7bd791eb539d7eec4dfcd36fc4ec15cec5131e7e
Mathias Sønderskov Schaltz <19936679+Sonderwoods@users.noreply.github.com>
2023-06-20 11:31:39 +0200

    1.1.0

--- GH_ImageClipboard/GH_Convert_To_Bitmap.cs
+++ GH_ImageClipboard/GH_Convert_To_Bitmap.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Drawing;
+using System.IO;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
 using Grasshopper.Kernel;
 using Rhino.Geometry;
 using Sonderwoods;
@@ -13,9 +17,9 @@ namespace GH_ImageClipboard
         /// Initializes a new instance of the GH_Convert_To_Bitmap class.
         /// </summary>
         public GH_Convert_To_Bitmap()
-          : base("get inner bitmap", "Nickname",
-              "Description",
-              "Category", "Subcategory")
+          : base("get inner bitmap TEST", "Get Inner Img",
+              "Part of GH_ImageClipboard component",
+              "Misc", "Misc")
         {
         }
 
@@ -32,7 +36,8 @@ namespace GH_ImageClipboard
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            pManager.AddGenericParameter("out", "out", "out", GH_ParamAccess.item);
+            pManager.AddGenericParameter("bitmap", "bitmap", "bitmap", GH_ParamAccess.item);
+            pManager.AddGenericParameter("imagesource", "imagesource", "imagesource (BitmapImage) - useful for human UI", GH_ParamAccess.item);
         }
 
         /// <summary>
@@ -46,10 +51,25 @@ namespace GH_ImageClipboard
             DA.GetData(0, ref goo);
 
 
+
             DA.SetData(0, goo.Bitmap);
+
+            MemoryStream ms = new MemoryStream();
+            goo.Bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.EndInit();
+
+
+            DA.SetData(1, image);
 
 
         }
+
+
+
 
         /// <summary>
         /// Provides an Icon for the component.
